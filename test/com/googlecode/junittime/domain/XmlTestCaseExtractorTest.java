61cbbafd0366897c4f91b2207ad9c3a3aaa5ed87
Derek Yang <hyang@thoughtworks.com>
2009-03-12 12:13:00 +0800

    Separating package name from class name (forgot to commit this file)

--- test/com/googlecode/junittime/domain/XmlTestCaseExtractorTest.java
+++ test/com/googlecode/junittime/domain/XmlTestCaseExtractorTest.java
@@ -12,6 +12,7 @@ public class XmlTestCaseExtractorTest {
     private static final String TEST_DATA = "test/data";
     private static final String FILE_WITH_ONE_TESTCASE = "TEST-com.googlecode.junittime.domain.TestCaseRepositoryTest.xml";
     private static final String FILE_WITH_TWO_TESTCASES = "TEST-com.googlecode.junittime.JunitTimeTest.xml";
+    private static final String FILE_WITH_NO_PACKAGENAME = "TEST-com.googlecode.junittime.domain.reporting.HtmlReportGeneratorTest.xml";
 
     @Test
     public void shouldExtractFromXmlTestResultFile() throws ExtractionException {
@@ -22,7 +23,7 @@ public class XmlTestCaseExtractorTest {
         List<TestCase> testCases = repository.byDurationDesc();
         assertThat(testCases.size(), is(1));
         assertThat(testCases.get(0),
-                is(new TestCase("com.googlecode.junittime.domain.TestCaseRepositoryTest", "shouldSortTestsAccordingToDurationDesc", 0.0020)));
+                is(new TestCase("com.googlecode.junittime.domain", "TestCaseRepositoryTest", "shouldSortTestsAccordingToDurationDesc", 0.0020)));
     }
 
     @Test
@@ -34,10 +35,9 @@ public class XmlTestCaseExtractorTest {
         List<TestCase> testCases = repository.byDurationDesc();
         assertThat(testCases.size(), is(2));
         assertThat(testCases.get(0),
-                is(new TestCase("com.googlecode.junittime.JunitTimeTest", "shouldGenerateReport", 0.029)));
+                is(new TestCase("com.googlecode.junittime", "JunitTimeTest", "shouldGenerateReport", 0.029)));
         assertThat(testCases.get(1),
-                is(new TestCase("com.googlecode.junittime.JunitTimeTest", "shouldCreateToDirIfNotExist", 0.0020)));
-
+                is(new TestCase("com.googlecode.junittime", "JunitTimeTest", "shouldCreateToDirIfNotExist", 0.0020)));
     }
 
     @Test
@@ -49,5 +49,17 @@ public class XmlTestCaseExtractorTest {
             fail("should have thrown when file is not found");
         } catch (ExtractionException e) {
         }
+    }
+
+    @Test
+    public void shouldExtractTestCasesWithoutPackageName() throws ExtractionException {
+        XmlTestCaseExtractor extractor = new XmlTestCaseExtractor(new File(TEST_DATA, FILE_WITH_NO_PACKAGENAME));
+        TestCaseRepository repository = new TestCaseRepository();
+        extractor.extractTo(repository);
+
+        List<TestCase> testCases = repository.byDurationDesc();
+        assertThat(testCases.size(), is(1));
+        assertThat(testCases.get(0),
+                is(new TestCase("", "HtmlReportGeneratorTest", "shouldGenerateReportThatHighlightSlowTests", 0.001)));
     }
 }
